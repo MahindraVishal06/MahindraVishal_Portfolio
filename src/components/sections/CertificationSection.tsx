de8aae33852b74eab287c5c3e7a6d5911d546d44
MahindraVishal06 <dmahindravishal@gmail.com>
2026-08-30 12:27:52 +0530

    Update CertificationSection.tsx

--- src/components/sections/CertificationSection.tsx
+++ src/components/sections/CertificationSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { motion } from 'framer-motion';
-import { FiAward, FiCheckCircle } from 'react-icons/fi';
+import { FiAward } from 'react-icons/fi';
 
 export const CertificationSection: React.FC = () => {
   const certificationPhotos = [
